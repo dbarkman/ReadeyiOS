007f26f4f8dfe65a6b58a18b8f2ac3c4f01c33ef
David Barkman <david.barkman13@gmail.com>
2013-02-05 17:03:04 -0700

    Starting a feedback view

---
+++ Readey/FeedbackViewController.h
@@ -0,0 +1,13 @@
+//
+//  FeedbackViewController.h
+//  Readey
+//
+//  Created by David Barkman on 2/5/13.
+//  Copyright (c) 2013 RealSimpleApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedbackViewController : UIViewController
+
+@end
---
+++ Readey/FeedbackViewController.m
@@ -0,0 +1,38 @@
+//
+//  FeedbackViewController.m
+//  Readey
+//
+//  Created by David Barkman on 2/5/13.
+//  Copyright (c) 2013 RealSimpleApps. All rights reserved.
+//
+
+#import "FeedbackViewController.h"
+
+@interface FeedbackViewController ()
+
+@end
+
+@implementation FeedbackViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
